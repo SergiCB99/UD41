1540ef9c2de001f60d6d8fb27433ffe5b5cc4796
SergiCB99 <sergicampanalesbalana@gmail.com>
2021-09-12 12:58:25 +0200

    Create superheroe.model.ts

---
+++ CrudSuperheroes/src/app/models/superheroe.model.ts
@@ -0,0 +1,13 @@
+export class Superheroe {
+  id?: any;
+  name?:string;
+  short_name?:string;
+  alt_name?:string;
+  role?:string;
+  new_role?:string;
+  type?:string;
+  release_date?:string;
+  rework_date?:string;
+  attribute_id?:number;
+  translations?:string[];
+}
